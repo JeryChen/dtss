6ec17ba2dd5cdae38cddee38224f7d6b8cfa68ef
xy <de_fu_chen@163.com>
2020-08-13 23:29:02 +0800

    测试下github提交

--- dtss-infrastructure/src/main/java/com/df/dtss/config/ColaConfig.java
+++ dtss-infrastructure/src/main/java/com/df/dtss/config/ColaConfig.java
@@ -6,7 +6,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 /**
+ *
  * Configuration for COLA framework
+ *
  */
 @Configuration
 @ComponentScan(value = {"com.xy.cola","com.df.dtss"})
